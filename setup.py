6b07e7ba1ac7cbd9feea958063f59138a6711686
sound5arya <94166786+sound5arya@users.noreply.github.com>
2021-11-12 11:50:00 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='ga78koxa',
+      version='0.1',
+      author='DSSS',
+      author_email='soundarya.soundarresan@fau.de',
+      packages=find_packages(),
+      install_requires=['numpy', 'Pillow', 'ipywidgets'])
