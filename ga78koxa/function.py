49e2ab1dd4295e8314049db4ce8847f4e31fc784
sound5arya <94166786+sound5arya@users.noreply.github.com>
2021-11-13 17:33:01 +0100

    imshow function definition

--- ga78koxa/function.py
+++ ga78koxa/function.py
@@ -2,12 +2,7 @@ import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
 def imshow(X, resize=None):
-
-  """
-  You should create a way to resize an image from an array X.
-  The use of widgets is optional but you can take a look to interact.
-  We should be able to install this package in Google Colab from your Git
-  repo.
-  """
-
-pass
+  pil_image=Image.fromarray(X)
+  pil_image_resize = pil_image.resize(resize)
+  #pil_image_resize.show()
+  return pil_image_resize
